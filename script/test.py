2dc06a0196f1feebc6d2e0169e86ef999007f66e
weakit <vsroshan86@gmail.com>
2024-03-03 12:52:48 +0530

    Edits

--- script/test.py
+++ script/test.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
-from tvmc import MotionController, DoF, ControlMode
+
 import rospy 
 import time
 from std_msgs.msg import Float32MultiArray, Int32MultiArray, Float64
 from geometry_msgs.msg import Vector3
-import depthai as dai
-import numpy as np
 from pathlib import Path
 import cv2
 
-nnBlobPath = str((Path(__file__).parent / Path('../models/my_openvino_blob.blob')).resolve().absolute())
+from tvmc import MotionController, DoF, ControlMode
 
+# nnBlobPath = str((Path(__file__).parent / Path('../models/my_openvino_blob.blob')).resolve().absolute())
+relPos = Vector3()
 
-if not Path(nnBlobPath).exists():
-    import sys
-    raise FileNotFoundError(f'Required file/s not found')
+def update_curr_state(x):
+    global relPos
+    relPos.x = x.x
+    relPos.y = x.y  
+    relPos.z = x.z
 
-labelMap = [
-            "Cans",
-            "bottle",
-            "paper"
-        ]
+# if not Path(nnBlobPath).exists():
+#     import sys
+#     raise FileNotFoundError(f'Required file/s not found')
 
-syncNN = True
+# labelMap = [
+#     "Cans",
+#     "bottle",
+#     "paper"
+# ]
 
-YAW_KP = 1
-YAW_KI = 1
-YAW_KD = 1
+# syncNN = True
+
+YAW_KP = 0.6
+YAW_KI = 0.012
+YAW_KD = 0.05
 YAW_TARGET = 0
-YAW_ACCEPTABLE_ERROR = 1
+YAW_ACCEPTABLE_ERROR = .05
 
-HEAVE_KP = 1
-HEAVE_KI = 1
-HEAVE_KD = 1
-HEAVE_TARGET = 20
-HEAVE_ACCEPTABLE_ERROR = 1
+HEAVE_KP = 100
+HEAVE_KI = 13
+HEAVE_KD = 0
+HEAVE_TARGET = 1.5
+HEAVE_ACCEPTABLE_ERROR = 0.05
 
 curr_yaw = 0
 
@@ -42,218 +48,49 @@ def orientation(x):
     global curr_yaw
     m.set_current_point(DoF.ROLL, x.x)
     m.set_current_point(DoF.PITCH, x.y)
-    m.set_current_point(DoF.YAW, x.z)
-    curr_yaw = x.z
-
+    m.set_current_point(DoF.YAW, x.z+180)
+    curr_yaw = x.z+180
+    print("Curr Yaw :",curr_yaw,end='\r')
 
 def depth(d):
+    # print("Depth : ", d.data, end="\r")
     m.set_current_point(DoF.HEAVE, d.data)
-    set("Depth", d.data)
-
+    
 m = MotionController()
-
-m.set_control_mode(DoF.YAW,ControlMode.CLOSED_LOOP)
-m.set_control_mode(DoF.HEAVE,ControlMode.CLOSED_LOOP)
-m.set_pid_constants(DoF.YAW, YAW_KP, YAW_KI, YAW_KD, YAW_ACCEPTABLE_ERROR)
-m.set_target_point(DoF.YAW, YAW_TARGET)
-m.set_pid_constants(DoF.HEAVE, HEAVE_KP, HEAVE_KI, HEAVE_KD, HEAVE_ACCEPTABLE_ERROR)
-m.set_target_point(DoF.HEAVE, HEAVE_TARGET)
 m.start()
+m.set_control_mode(DoF.YAW, ControlMode.CLOSED_LOOP)
+m.set_control_mode(DoF.HEAVE, ControlMode.CLOSED_LOOP)
+m.set_pid_constants(DoF.HEAVE, HEAVE_KP, HEAVE_KI, HEAVE_KD, HEAVE_ACCEPTABLE_ERROR)
+m.set_pid_constants(DoF.YAW, YAW_KP, YAW_KI, YAW_KD, YAW_ACCEPTABLE_ERROR)
+m.set_pid_limits(DoF.HEAVE, -2, 2, -50, 50)
+m.set_target_point(DoF.HEAVE, HEAVE_TARGET)
+m.set_current_point(DoF.HEAVE, 0.09)
+m.set_target_point(DoF.YAW, YAW_TARGET)
 
-
-rospy.Subscriber("/euler_orientation", Vector3, orientation)
+rospy.Subscriber("/diagnostics/orientation", Vector3, orientation)
 rospy.Subscriber("/depth_data", Float64, depth)
-pipeline = dai.Pipeline()
-camRgb = pipeline.create(dai.node.ColorCamera)
-spatialDetectionNetwork = pipeline.create(dai.node.YoloSpatialDetectionNetwork)
-monoLeft = pipeline.create(dai.node.MonoCamera)
-monoRight = pipeline.create(dai.node.MonoCamera)
-stereo = pipeline.create(dai.node.StereoDepth)
-nnNetworkOut = pipeline.create(dai.node.XLinkOut)
+rospy.Subscriber("/localization/gate", Vector3, update_curr_state)
 
-xoutRgb = pipeline.create(dai.node.XLinkOut)
-xoutNN = pipeline.create(dai.node.XLinkOut)
-xoutDepth = pipeline.create(dai.node.XLinkOut)
-
-xoutRgb.setStreamName("rgb")
-xoutNN.setStreamName("detections")
-xoutDepth.setStreamName("depth")
-nnNetworkOut.setStreamName("nnNetwork")
-
-# Properties
-camRgb.setPreviewSize(416, 416)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-camRgb.setInterleaved(False)
-camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-
-monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-monoLeft.setCamera("left")
-monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-monoRight.setCamera("right")
-
-stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-# Align depth map to the perspective of RGB camera, on which inference is done
-stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
-stereo.setOutputSize(monoLeft.getResolutionWidth(), monoLeft.getResolutionHeight())
-stereo.setSubpixel(True)
-
-spatialDetectionNetwork.setBlobPath(nnBlobPath)
-spatialDetectionNetwork.setConfidenceThreshold(0.5)
-spatialDetectionNetwork.input.setBlocking(False)
-spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
-spatialDetectionNetwork.setDepthLowerThreshold(100)
-spatialDetectionNetwork.setDepthUpperThreshold(5000)
-
-# Yolo specific parameters
-spatialDetectionNetwork.setNumClasses(3)
-spatialDetectionNetwork.setCoordinateSize(4)
-spatialDetectionNetwork.setAnchors([
-                10.0,
-                13.0,
-                16.0,
-                30.0,
-                33.0,
-                23.0,
-                30.0,
-                61.0,
-                62.0,
-                45.0,
-                59.0,
-                119.0,
-                116.0,
-                90.0,
-                156.0,
-                198.0,
-                373.0,
-                326.0
-            ])
-spatialDetectionNetwork.setAnchorMasks({
-                "side52": [
-                    0,
-                    1,
-                    2
-                ],
-                "side26": [
-                    3,
-                    4,
-                    5
-                ],
-                "side13": [
-                    6,
-                    7,
-                    8
-                ]
-            })
-spatialDetectionNetwork.setIouThreshold(0.5)
-
-# Linking
-monoLeft.out.link(stereo.left)
-monoRight.out.link(stereo.right)
-
-camRgb.preview.link(spatialDetectionNetwork.input)
-
-if syncNN:
-    spatialDetectionNetwork.passthrough.link(xoutRgb.input)
-else:
-    camRgb.preview.link(xoutRgb.input)
-
-spatialDetectionNetwork.out.link(xoutNN.input)
-
-stereo.depth.link(spatialDetectionNetwork.inputDepth)
-spatialDetectionNetwork.passthroughDepth.link(xoutDepth.input)
-spatialDetectionNetwork.outNetwork.link(nnNetworkOut.input)
-
-with dai.Device(pipeline) as device:
-
-    # Output queues will be used to get the rgb frames and nn data from the outputs defined above
-    previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-    detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
-    depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
-    networkQueue = device.getOutputQueue(name="nnNetwork", maxSize=4, blocking=False)
-
-    startTime = time.monotonic()
-    counter = 0
-    fps = 0
-    color = (255, 255, 255)
-    printOutputLayersOnce = True
-
-    while True:
-        inPreview = previewQueue.get()
-        inDet = detectionNNQueue.get()
-        depth = depthQueue.get()
-        inNN = networkQueue.get()
-
-        if printOutputLayersOnce:
-            toPrint = 'Output layer names:'
-            for ten in inNN.getAllLayerNames():
-                toPrint = f'{toPrint} {ten},'
-            print(toPrint)
-            printOutputLayersOnce = False
-
-        frame = inPreview.getCvFrame()
-        depthFrame = depth.getFrame() # depthFrame values are in millimeters
-
-        depth_downscaled = depthFrame[::4]
-        if np.all(depth_downscaled == 0):
-            min_depth = 0  # Set a default minimum depth value when all elements are zero
-        else:
-            min_depth = np.percentile(depth_downscaled[depth_downscaled != 0], 1)
-        max_depth = np.percentile(depth_downscaled, 99)
-        depthFrameColor = np.interp(depthFrame, (min_depth, max_depth), (0, 255)).astype(np.uint8)
-        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
-
-        counter+=1
-        current_time = time.monotonic()
-        if (current_time - startTime) > 1 :
-            fps = counter / (current_time - startTime)
-            counter = 0
-            startTime = current_time
-
-        detections = inDet.detections
-
-        # If the frame is available, draw bounding boxes on it and show the frame
-        height = frame.shape[0]
-        width  = frame.shape[1]
-        for detection in detections:
-            roiData = detection.boundingBoxMapping
-            roi = roiData.roi
-            roi = roi.denormalize(depthFrameColor.shape[1], depthFrameColor.shape[0])
-            topLeft = roi.topLeft()
-            bottomRight = roi.bottomRight()
-            xmin = int(topLeft.x)
-            ymin = int(topLeft.y)
-            xmax = int(bottomRight.x)
-            ymax = int(bottomRight.y)
-            xmean = int((xmin + xmax)/2)
-            ymean = int((ymin+ymax)/2)
-            xcenter = width//2
-            ycenter = height//2
-            cv2.circle(depthFrame,(xcenter,ycenter),2,color,1)
-            cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, 1)
-
-            # Denormalize bounding box
-            x1 = int(detection.xmin * width)
-            x2 = int(detection.xmax * width)
-            y1 = int(detection.ymin * height)
-            y2 = int(detection.ymax * height)
-            try:
-                label = labelMap[detection.label]
-            except:
-                label = detection.label
-            cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, "{:.2f}".format(detection.confidence*100), (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, f"X: {int(detection.spatialCoordinates.x)} mm", (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, f"Y: {int(detection.spatialCoordinates.y)} mm", (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, f"Z: {int(detection.spatialCoordinates.z)} mm", (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-
-            cv2.rectangle(frame, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
-
-            cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
-            cv2.imshow("depth", depthFrameColor)
-            cv2.imshow("rgb", frame)
-            if xcenter-xmean >10 :  m.set_target_point(DoF.YAW, curr_yaw+1)
-            elif xmean - xcenter >10 :  m.set_target_point(DoF.YAW, curr_yaw-1)
-            break
-
-        if cv2.waitKey(1) == ord('q'):
-            break
+m.set_control_mode(DoF.YAW, ControlMode.CLOSED_LOOP)
+m.set_control_mode(DoF.SURGE, ControlMode.OPEN_LOOP)
+m.set_thrust(DoF.SURGE, 0)    
+# m.set_thrust(DoF.YAW, 0)
+rate = rospy.Rate(5)
+import math
+time.sleep(1)
+curr_target = curr_yaw
+# curr_target = curr_target + 90
+# m.set_target_point(DoF.YAW,90)
+# rospy.spin()
+while rospy.is_shutdown() == False:
+    print(curr_target)
+    if math.degrees(math.atan2(relPos.y, relPos.x)) < 90:
+        curr_target = min(curr_target + .1, 180)
+        
+        m.set_target_point(DoF.YAW, curr_target)
+    elif math.degrees(math.atan2(relPos.y, relPos.x)) > 90:
+        m.set_target_point(DoF.YAW, curr_target)
+        curr_target = max(curr_target - .1, -180)
+    # if curr_target<60 : curr_target+=.1
+    # m.set_target_point(DoF.YAW, curr_target)
+    rate.sleep()
